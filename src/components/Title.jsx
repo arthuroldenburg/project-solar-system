e91df3e5ce8445ec1bbb4a81013dd8e41869cca2
Arthur Murilo Oldenburg <oldenburgarthurmurilo@gmail.com>
2022-10-21 15:23:45 -0300

    Req 7 Feita

---
+++ src/components/Title.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Title extends Component {
+  render() {
+    return (
+      <h2>Aqui vai a prop mec</h2>
+    );
+  }
+}
+
+export default Title;
