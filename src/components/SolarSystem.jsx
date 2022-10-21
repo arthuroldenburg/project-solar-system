5e1174cf39ba1186b58a8f09264da0e344a9f0f6
Arthur Murilo Oldenburg <oldenburgarthurmurilo@gmail.com>
2022-10-21 15:19:16 -0300

    Req 2 feito

---
+++ src/components/SolarSystem.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class SolarSystem extends Component {
+  render() {
+    return (
+      <div data-testid="solar-system"> só para n ficar em braco e o lint reclamar</div>
+    );
+  }
+}
+
+export default SolarSystem;
